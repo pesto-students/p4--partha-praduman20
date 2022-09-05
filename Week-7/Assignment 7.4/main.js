1b4a966c8beaa57651706718d4a0d99742122893
praduman20 <pschundawat4080@gmail.com>
2022-09-06 00:42:10 +0530

    Assignment 7.4 completed

---
+++ Week-7/Assignment 7.4/main.js
@@ -0,0 +1,22 @@
+function parenthesisChecker(string){
+    let stack =[];
+    for(let i =0;i<string.length ;i++){
+        let element  = string[i];
+        if(element == "(" || element == "[" || element == "{"){
+            stack.push(element);
+            continue;
+        }
+        if(element == ")" || element == "]" || element == "}"){
+            stack.pop();
+        }
+    }
+    if(stack.length === 0){
+        return true;
+    }
+    return false;
+}
+
+let string = "([{}])";
+console.log(parenthesisChecker(string));
+let string2 = "([]";
+console.log(parenthesisChecker(string2));
